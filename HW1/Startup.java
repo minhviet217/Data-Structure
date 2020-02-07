fd7952eb5a4b49adef86a9fa1362152dd7ee9508
minhviet217 <nickm8704@gmail.com>
2020-02-06 18:14:05 -0600

    HW1, prbl 3

---
+++ HW1/Startup.java
@@ -0,0 +1,17 @@
+/**
+* A starup class that acts as a sandbox to make use of our Infix to Postfix conversion 
+*
+* @name		Viet Nguyen, Joshua Martin
+* @version	02/06/2020
+*/
+import java.util.*;
+import java.util.Iterator;
+public class Startup<T>{
+	
+	public static void main(String[] args){
+	
+		
+		
+	}
+	
+}
