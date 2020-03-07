c06f782fecfbc1c8e41771bc1a1e9e81330a35f7
jdmarti3 <jdmarti3@uno.edu>
2020-03-07 16:52:04 -0600

    Check this because it failed the tester!

--- HW2/BinarySearchTree.java
+++ HW2/BinarySearchTree.java
@@ -1,4 +1,69 @@
 
-public class BinarySearchTree(){
+public class BinarySearchTree<T extends Comparable<T>> extends BinaryTree<T>{
+	BinaryNode<T> root = null;
+	
+	public BinarySearchTree(){
+	}
+	
+	public BinarySearchTree(T[] seq){
+		super();
+	}
+	
+	public BinarySearchTree(T[] seq, T nullSymbol){
+		super();
+	}
+		
+	
+	public void insert(T value){
+		BinaryNode<T> currentNode = root;
+
+		while(currentNode.getData() != null){
+			int compareResult = value.compareTo(currentNode.getData());
+			if(compareResult > 0){
+				currentNode = currentNode.getRightNode();
+			}
+			else if(compareResult < 0){
+				currentNode = currentNode.getLeftNode();
+			}else{
+				return;
+			}
+		}
+		currentNode.setData(value);		
+	}
+	
+	public void remove(T value){
+		BinaryNode<T> currentNode = root;
+
+		while(currentNode.getData() != null){
+			int compareResult = value.compareTo(currentNode.getData());
+			if(compareResult > 0){
+				currentNode = currentNode.getRightNode();
+			}
+			else if(compareResult < 0){
+				currentNode = currentNode.getLeftNode();
+			}else{
+				return;
+			}
+		}
+		currentNode.setData(null);	
+	}
+	
+	public boolean contains(T value){
+		BinaryNode<T> currentNode = root;
+		
+		while(currentNode.getData() != null){
+			int compareResult = value.compareTo(currentNode.getData());
+			if(compareResult > 0){
+				currentNode = currentNode.getRightNode();
+			}
+			else if(compareResult < 0){
+				currentNode = currentNode.getLeftNode();
+			}else{
+				return true;
+			}
+		}
+		return false;
+		
+	}
 
 }
